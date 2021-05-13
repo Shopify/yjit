d8e785b910a9405ef4d445f51da0a183c4a84cf6
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2021-05-13 13:25:12 -0400

    Try to fix MJIT symbol clash with cargo cult

--- vm_insnhelper.c
+++ vm_insnhelper.c
@@ -2238,7 +2238,7 @@ rb_simple_iseq_p(const rb_iseq_t *iseq)
 	   iseq->body->param.flags.has_block == FALSE;
 }
 
-bool
+MJIT_FUNC_EXPORTED bool
 rb_iseq_only_optparam_p(const rb_iseq_t *iseq)
 {
     return iseq->body->param.flags.has_opt == TRUE &&
@@ -2250,7 +2250,7 @@ rb_iseq_only_optparam_p(const rb_iseq_t *iseq)
            iseq->body->param.flags.has_block == FALSE;
 }
 
-bool
+MJIT_FUNC_EXPORTED bool
 rb_iseq_only_kwparam_p(const rb_iseq_t *iseq)
 {
     return iseq->body->param.flags.has_opt == FALSE &&
